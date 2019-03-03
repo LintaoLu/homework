42b47f0d31133fefd9fa3d869944feeb9c46156d
LintaoLu <43651699+LintaoLu@users.noreply.github.com>
2019-03-03 17:03:24 -0600

    Update CPU_Benchmark.c

--- CPU_Benchmark.c
+++ CPU_Benchmark.c
@@ -2,7 +2,7 @@
 #include<pthread.h>
 #include<stdlib.h>
 
-#define GOAL 2000000000
+#define GOAL 100000000
 
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 int counter = 0;
@@ -13,16 +13,7 @@ void executeThread(int number);
 int main()
 {
   pthread_mutex_init(&mutex1, NULL);
-  printf("1 threads are executing, please wait...\n");
-  clock_t begin = clock();
-  while(counter < GOAL)
-  {
-    counter++;
-  }
-  clock_t end = clock();
-  double time = (double)(end-begin) / CLOCKS_PER_SEC;
-  double efficiency = (double)(GOAL / time);
-  printf("1 threads finish, counter = %d, benchmark = %f i/s\n", counter, efficiency);
+  executeThread(1);
   executeThread(2);
   executeThread(4);
   executeThread(8);
@@ -32,13 +23,14 @@ int main()
 void executeThread(int number)
 {
   printf("%d threads are executing, please wait...\n", number);
+  int i, j;
   counter = 0;
   pthread_t thread_id[number];
-  int i, j;
+  //Set clock.
   clock_t begin = clock();
   for(i = 0; i < number; i++)
   {
-    pthread_create(&thread_id[i], NULL, add, &thread_id[i]);
+    pthread_create(&thread_id[i], NULL, add, &number);
   }
 
   for(j = 0; j < number; j++)
@@ -53,16 +45,14 @@ void executeThread(int number)
 
 void *add(void *args)
 {
-  pthread_mutex_lock(&mutex1);
-
   int round = 0;
-  int *thread_args = (int*) args;
-  printf("This is thread: %d\n", *thread_args);
-  while(counter < GOAL && round < GOAL/2)
+  int *number = (int*) args;
+  while(counter < GOAL && round < GOAL/(*number))
   {
+    pthread_mutex_lock(&mutex1);
     round++;
+    //printf("%d threads running\n", (*number));
     counter++;
+    pthread_mutex_unlock(&mutex1);
   }
-
-  pthread_mutex_unlock(&mutex1);
 }
