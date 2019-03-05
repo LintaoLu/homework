56607f395fa0717558fc6b0a39828fc2c504e2e0
LintaoLu <43651699+LintaoLu@users.noreply.github.com>
2019-03-05 16:43:42 -0600

    Update Matrix.c

--- Matrix.c
+++ Matrix.c
@@ -1,89 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int **createMatrix(int length);
-void initMatrix(int** matrix, int length);
-void setMatrix(int **matrix, int length, int number);
-void displayMatrix(int** matrix, int length);
-int **multiplyMatrix(int** left, int** right, int length);
+#define DOUBLE 0
+#define FLOAT  1
+
+void **createMatrix(int length, int type, void* number);
+void displayMatrix(void** matrix, int length, int type);
+void **multiplyMatrix(void** left_1, void** right_1, int length, int type);
 
 int main()
 {
-  int length = 4;
-  int **left = createMatrix(length);
-  int **right = createMatrix(length);
-  setMatrix(left, length, 2);
-  setMatrix(right, length, 1);
-  displayMatrix(left, length);
-  printf("\n");
-  displayMatrix(right, length);
-  printf("\n");
-  displayMatrix(multiplyMatrix(left, right, length), length);
-  return 0;
+	double a = 1, b = 2;
+	float c = 1;
+	int length = 5;
+	double** left = (double**)createMatrix(length, DOUBLE, &a);
+	displayMatrix((void**)left, length, DOUBLE);
+	printf("\n");
+	double** right = (double**)createMatrix(length, DOUBLE, &b);
+	displayMatrix((void**)right, length, DOUBLE);
+	printf("\n");
+	double** answer = (double**)multiplyMatrix((void**)left, (void**)right, length, DOUBLE);
+	displayMatrix((void**)answer, length, DOUBLE);
+	return 0;
 }
 
-int **createMatrix(int length)
+void **createMatrix(int length, int type, void* number)
 {
-  int **matrix = (int**)malloc(sizeof(*matrix)*length);
-  for (int i = 0; i < length; i++)
-    matrix[i] = (int*)malloc(sizeof(int)*length);
-  initMatrix(matrix, length);
-  return matrix;
+	if (type == DOUBLE)
+	{
+		double element = *((double*)number);
+		double **martrix = (double**)malloc(sizeof(*martrix)*length);
+		for (int i = 0; i < length; i++)
+		{
+			martrix[i] = (double*)malloc(sizeof(double)*length);
+			for (int j = 0; j < length; j++)
+				martrix[i][j] = element++;
+		}
+		return (void**)martrix;
+	}
+	if (type == FLOAT)
+	{
+		float element = *((float*)number);
+		float **martrix = (float**)malloc(sizeof(*martrix)*length);
+		for (int i = 0; i < length; i++)
+		{
+			martrix[i] = (float*)malloc(sizeof(float)*length);
+			for (int j = 0; j < length; j++)
+				martrix[i][j] = element++;
+		}
+		return (void**)martrix;
+	}
+	return NULL;
 }
 
-void initMatrix(int** matrix, int length)
+void displayMatrix(void** matrix, int length, int type)
 {
-  int i, j;
-  for (i = 0; i < length; i++)
-  {
-    for (j = 0; j < length; j++)
-      matrix[i][j] = 0;
-  }
+	if (type == DOUBLE)
+	{
+		double** answer = (double**)matrix;
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < length; j++)
+				printf("%f\t", answer[i][j]);
+			printf("\n");
+		}
+		return;
+	}
+
+	if (type == FLOAT)
+	{
+		float** answer = (float**)matrix;
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < length; j++)
+				printf("%f\t", answer[i][j]);
+			printf("\n");
+		}
+		return;
+	}
+	printf("Error: unknow type!");
 }
 
-void setMatrix(int **matrix, int length, int number)
+void **multiplyMatrix(void** left_1, void** right_1, int length, int type)
 {
-  if (number == 0) return;
-  int i, j;
-  for (i = 0; i < length; i++)
-  {
-    if (number % 2 == 0)
-    {
-      for (j = 0; j < length; j += 2)
-        matrix[i][j] = number;
-    }
-    else
-    {
-      for (j = 1; j < length; j += 2)
-        matrix[i][j] = number;
-    }
-  }
+	if (type == DOUBLE)
+	{
+		double **left = (double**)left_1;
+		double **right = (double**)right_1;
+		double number = 0;
+		double **answer = (double**)createMatrix(length, type, &number);
+		int i, j, k;
+		for (i = 0; i < length; i++)
+		{
+			for (k = 0; k < length; k++)
+			{
+				for (j = 0; j < length; j++)
+					number += (left[i][j] * right[j][i]);
+				answer[i][k] = number;
+				number = 0;
+			}
+		}
+		return (void**)answer;
+	}
+
+	if (type == FLOAT)
+	{
+		float **left = (float**)left_1;
+		float **right = (float**)right_1;
+		float number = 0;
+		float **answer = (float**)createMatrix(length, type, &number);
+		int i, j, k;
+		for (i = 0; i < length; i++)
+		{
+			for (k = 0; k < length; k++)
+			{
+				for (j = 0; j < length; j++)
+					number += (left[i][j] * right[j][i]);
+				answer[i][k] = number;
+				number = 0;
+			}
+		}
+		return (void**)answer;
+	}
+	return NULL;
 }
 
-void displayMatrix(int** matrix, int length)
-{
-  int i, j;
-  for (i = 0; i < length; i++)
-  {
-    for (j = 0; j < length; j++)
-      printf("%d\t", matrix[i][j]);
-    printf("\n");
-  }
-}
-
-int **multiplyMatrix(int** left, int** right, int length)
-{
-  int** result = createMatrix(length);
-  int i, j, k, temp = 0;
-  for (i = 0; i < length; i++)
-  {
-    for (k = 0; k < length; k++)
-    {
-      for (j = 0; j < length; j++)
-        temp += (left[i][j] * right[j][i]);
-          result[i][k] = temp;
-          temp = 0;
-    }
-  }
-  return result;
-}
