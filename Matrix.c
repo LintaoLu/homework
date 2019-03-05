0220b1e3806e99c7cc97e78a4610f60ad6580016
LintaoLu <43651699+LintaoLu@users.noreply.github.com>
2019-03-05 15:46:39 -0600

    Update Matrix.c

--- Matrix.c
+++ Matrix.c
@@ -1,23 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int **createMatrix(int length);
+void initMatrix(int** matrix, int length);
+void setMatrix(int **matrix, int length, int number);
+void displayMatrix(int** matrix, int length);
+int **multiplyMatrix(int** left, int** right, int length);
+
+int main()
+{
+	int length = 4;
+	int **left = createMatrix(length);
+	int **right = createMatrix(length);
+	setMatrix(left, length, 2);
+	setMatrix(right, length, 1);
+	displayMatrix(left, length);
+	printf("\n");
+	displayMatrix(right, length);
+	printf("\n");
+	displayMatrix(multiplyMatrix(left, right, length), length);
+	return 0;
+}
+
 int **createMatrix(int length)
 {
 	int **matrix = (int**)malloc(sizeof(*matrix)*length);
 	for (int i = 0; i < length; i++)
 		matrix[i] = (int*)malloc(sizeof(int)*length);
+	initMatrix(matrix, length);
 	return matrix;
 }
 
-void initMatrix(int** matrix, int length, int number)
+void initMatrix(int** matrix, int length)
 {
 	int i, j;
 	for (i = 0; i < length; i++)
 	{
-		for (j = 0; j < length; j += 2)
-			matrix[i][j] = number;
-		for (j = 1; j < length; j += 2)
-			matrix[i][j] = abs(number - 1);
+		for (j = 0; j < length; j++)
+			matrix[i][j] = 0;
+	}
+}
+
+void setMatrix(int **matrix, int length, int number)
+{
+	if (number == 0) return;
+	int i, j;
+	for (i = 0; i < length; i++)
+	{
+		if (number % 2 == 0)
+		{
+			for (j = 0; j < length; j += 2)
+				matrix[i][j] = number;
+		}
+		else
+		{
+			for (j = 1; j < length; j += 2)
+				matrix[i][j] = number;
+		}
 	}
 }
 
@@ -27,7 +66,7 @@ void displayMatrix(int** matrix, int length)
 	for (i = 0; i < length; i++)
 	{
 		for (j = 0; j < length; j++)
-			printf("%d ", matrix[i][j]);
+			printf("%d\t", matrix[i][j]);
 		printf("\n");
 	}
 }
@@ -35,28 +74,16 @@ void displayMatrix(int** matrix, int length)
 int **multiplyMatrix(int** left, int** right, int length)
 {
 	int** result = createMatrix(length);
-	int i, j;
+	int i, j, k, temp = 0;
 	for (i = 0; i < length; i++)
 	{
-		for (j = 0; j < length; j++)
+		for (k = 0; k < length; k++)
 		{
-			result[i][j] = left[i][j] * right[j][i];
+			for (j = 0; j < length; j++)
+				temp += (left[i][j] * right[j][i]);
+			result[i][k] = temp;
+			temp = 0;
 		}
 	}
 	return result;
-}
-
-int main()
-{
-	int length = 4;
-	int **left = createMatrix(length);
-	int **right = createMatrix(length);
-	initMatrix(left, length, 1);
-	initMatrix(right, length, 0);
-	displayMatrix(left, length);
-	printf("\n");
-	displayMatrix(right, length);
-	printf("\n");
-	displayMatrix(multiplyMatrix(left, right, length), length);
-	return 0;
 }
